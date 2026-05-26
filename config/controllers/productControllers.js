e1c69e70b82624ea4c5a1b4dc683fe978cccc6bf
mahak <kaushikikes@gmail.com>
2026-05-26 19:39:52 +0530

    mvcpattern

---
+++ config/controllers/productControllers.js
@@ -0,0 +1,22 @@
+const product  = require('../models/productModel');
+
+const getProducts = async(requestAnimationFrame,res)=>{
+    try{
+        const allproducts = product.find();
+        if(!allproducts){
+            res.json({
+                message:"There is NO product"
+            })
+        }
+        res.status(200).json({
+            success:true,
+            products:allproducts,
+        })
+    }
+    catch(err){
+        res.status(500).json({
+            success:false,
+            message:"server error"                                        
+        })
+    }
+}
