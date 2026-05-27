7b6a511a068d4be6ce2d27adb024c3661cb6c8b3
mahak <kaushikikes@gmail.com>
2026-05-27 08:52:22 +0530

    routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {getProducts} = requier
+const {getProducts} = require('../controllers/productController')
 
 router.get('/products',)
 
