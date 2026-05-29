d3731a2c1c6bba4a7ddf8543546a088f47488e01
mahak <kaushikikes@gmail.com>
2026-05-29 20:14:02 +0530

    mvcpattern

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
-
+const productRoutes = require('./routes/productRoutes')
 const port = 3000;
 
 dotenv.config();
@@ -13,10 +13,10 @@ const PORT = process.env.PORT;
 app.use(express.json());
 
 
-app.get('/page', (req, res) => {
-  console.log("hello you are star");
+app.get('/', (req, res) => {
   res.send("hello welcome");
 })
+app.get('/api',productRoutes);
 
 app.listen(PORT, () => {
   console.log(`app is listening at ${PORT} port`);
