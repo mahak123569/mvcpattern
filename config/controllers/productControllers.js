80c8914a89dee807d0a5d44381f3587c8ba65e0d
mahak <kaushikikes@gmail.com>
2026-06-03 01:04:34 +0530

    mongo

--- config/controllers/productControllers.js
+++ config/controllers/productControllers.js
@@ -24,22 +24,48 @@ const getProducts = async (req, res) => {
         });
     }
 };
-const createProduct  = async (req,res)=>{
-    try{
-const {name,price,description,catgory}= req.body;
-const newProduct = new Product ({name,price,description ,catgory});
-await newProduct.save();
-res.status(200).json({
-    product: newProduct
-})
-    }
-    catch (err) {
+const createProduct = async (req, res) => {
+    try {
+        const { name, price, description, category } = req.body;
+        const newProduct = new Product({ name, price, description, category });
+
+        await newProduct.save();
+
+        return res.status(200).json({
+            product: newProduct,
+        });
+    } catch (err) {
         return res.status(500).json({
             success: false,
             message: err.message || "server error",
         });
-    
     }
-}
+};
 
-module.exports = { getProducts };
+const updateProduct = async (req, res) => {
+    try {
+        console.log("PUT REQUESTED")
+        const { id } = req.params;
+        const { name, price, description, category } = req.body;
+        const updatedProduct = await Product.findByIdAndUpdate(
+            id,
+            { name, price, description, category },
+            { new: true }
+        );
+        if(!updateProduct){
+            res.json({
+                message:"not find product"
+            })
+        }
+        res.status(200),json({
+            product: updatedProduct,
+        });
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            message: err.message || "server error",
+        });
+    }
+};
+
+module.exports = { getProducts, createProduct, updateProduct };
