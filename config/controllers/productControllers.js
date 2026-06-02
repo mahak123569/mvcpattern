766ed1857db723c46c912020082a302e5c2dd60d
mahak <kaushikikes@gmail.com>
2026-06-02 23:55:13 +0530

    mongo

--- config/controllers/productControllers.js
+++ config/controllers/productControllers.js
@@ -24,5 +24,22 @@ const getProducts = async (req, res) => {
         });
     }
 };
+const createProduct  = async (req,res)=>{
+    try{
+const {name,price,description,catgory}= req.body;
+const newProduct = new Product ({name,price,description ,catgory});
+await newProduct.save();
+res.status(200).json({
+    product: newProduct
+})
+    }
+    catch (err) {
+        return res.status(500).json({
+            success: false,
+            message: err.message || "server error",
+        });
+    
+    }
+}
 
 module.exports = { getProducts };
